1c9cc7708993569ffe47dd3a9b49b9f757174c4f
aburk3 <aburk3@gmail.com>
2019-06-24 05:34:05 -0500

    set up login method that generates the token

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -1,3 +1,5 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
@@ -43,7 +45,7 @@ namespace DatingApp.API.Controllers
 		[HttpPost("login")]
 		public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
 		{
-			var userFromRepo = await _repo.Login(userForLoginDto.Username, userForLoginDto.Password);
+			var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
 			if (userFromRepo == null)
 				return Unauthorized();
@@ -54,7 +56,26 @@ namespace DatingApp.API.Controllers
 				new Claim(ClaimTypes.Name, userFromRepo.Username)
 			};
 
-			var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
+			var key = new SymmetricSecurityKey(Encoding.UTF8
+				.GetBytes(_config.GetSection("AppSettings:Token").Value));
+
+			var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+			var tokenDescriptor = new SecurityTokenDescriptor
+			{
+				Subject = new ClaimsIdentity(claims),
+				Expires = DateTime.Now.AddDays(1),
+				SigningCredentials = creds
+			};
+
+			var tokenHandler = new JwtSecurityTokenHandler();
+
+			var token = tokenHandler.CreateToken(tokenDescriptor);
+
+			return Ok(new
+			{
+				token = tokenHandler.WriteToken(token)
+			});
 
 		}
 
