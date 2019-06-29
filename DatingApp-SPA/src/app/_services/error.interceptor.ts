f2815478fb8d046f78910b4a780741a35a864156
aburk3 <aburk3@gmail.com>
2019-06-29 13:54:02 -0500

    Added error interceptor provider

--- DatingApp-SPA/src/app/_services/error.interceptor.ts
+++ DatingApp-SPA/src/app/_services/error.interceptor.ts
@@ -1,9 +1,37 @@
-import {Injectable} from "@angular/core"
-import {HttpInterceptor, HttpRequest, HttpHandler, HttpEvent} from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpErrorResponse,
+  HTTP_INTERCEPTORS
+} from "@angular/common/http";
+import { Observable, throwError } from "rxjs";
+import { catchError } from "rxjs/operators";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-	intercept(req: HttpRequest<any>,);
-	}
-
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return next.handle(req).pipe(
+      catchError(error => {
+        if (error instanceof HttpErrorResponse) {
+          const applicationError: any = error.headers.get("Application-Error");
+          if (applicationError) {
+            console.error(applicationError);
+            return throwError(applicationError);
+          }
+        }
+      })
+    );
+  }
 }
+
+export const ErrorInterceptorProvider = {
+  provide: HTTP_INTERCEPTORS,
+  useClass: ErrorInterceptor,
+  multi: true
+};
